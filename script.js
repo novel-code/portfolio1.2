7c3d4f57d1fad6f28eeea2c6d2372ed36b3b3737
syed <novelcode0@gmail.com>
2021-08-27 22:12:35 +0530

    class list in event understood

--- script.js
+++ script.js
@@ -27,6 +27,8 @@ heroScroll.addEventListener("click", function (e) {
 document.querySelector(".nav__links").addEventListener("click", function (e) {
   e.preventDefault();
 
+  console.log(e.target.classList);
+
   // Matching strategy
   if (e.target.classList.contains("nav__link")) {
     const id = e.target.getAttribute("href");
